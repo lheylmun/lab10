a5314b6ff7996124320e42e92122b3f40bd8b3fe
lheylmun <lahe5495@colorado.edu>
2026-07-28 18:51:02 +0000

    Move database connection inside route

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ import os
 import psycopg2
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-conn = psycopg2.connect(DATABASE_URL)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Lauren in 3308'
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
