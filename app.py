cc29d543c231681d8f7e73a922054ccdb7721a72
lheylmun <lahe5495@colorado.edu>
2026-07-28 18:38:35 +0000

    Add PostgreSQL dependency and connection setup

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+import psycopg2
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 @app.route('/')
 def hello_world():
     return 'Hello, World from Lauren in 3308'
